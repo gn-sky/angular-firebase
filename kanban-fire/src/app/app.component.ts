2092f89831385945e01e1cb9a492ed5337bbca44
esky <eugene.sky.dev@gmail.com>
2023-07-22 15:03:14 +0300

    loop through Todos & display them in TaskComponent

--- kanban-fire/src/app/app.component.ts
+++ kanban-fire/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kanban-fire';
+
+  todo: Task[] = [
+    {
+      title: "Start",
+      description: "generate Angular app"
+    },
+    {
+      title: "Add Angular material",
+      description: "ng add @angular/material"
+    }
+  ]  
 }
