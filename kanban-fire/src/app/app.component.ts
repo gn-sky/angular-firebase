6c9d8e9aed8965a1e0fb6cb4fb1f46c0d1432217
esky <eugene.sky.dev@gmail.com>
2023-07-30 16:52:45 +0300

    task edit delete

--- kanban-fire/src/app/app.component.ts
+++ kanban-fire/src/app/app.component.ts
@@ -31,7 +31,30 @@ export class AppComponent {
 
   constructor(private dialog: MatDialog) {}
 
-  editTask = (todo: string, event: Task) => {};
+  editTask = (list: 'done' | 'todo' | 'inProgress', task: Task) => {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task,
+        enableDelete: true,
+      },
+    });
+    dialogRef
+      .afterClosed()
+      .pipe(take(1))
+      .subscribe((result: TaskDialogResult | undefined) => {
+        if (!result) {
+          return;
+        }
+        const dataList = this[list];
+        const taskIndex = dataList.indexOf(task);
+        if (result.delete) {
+          dataList.splice(taskIndex, 1);
+        } else {
+          dataList[taskIndex] = task;
+        }
+      });
+  };
 
   drop = (event: CdkDragDrop<Task[]>) => {
     if (event.previousContainer === event.container) {
@@ -52,7 +75,7 @@ export class AppComponent {
         task: {},
       },
     });
-    
+
     dialogRef
       .afterClosed()
       .pipe(take(1))
