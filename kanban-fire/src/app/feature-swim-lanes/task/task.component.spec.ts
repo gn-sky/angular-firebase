6a6759686f71c16f6322cfa1f1189a3f816b522d
esky <eugene.sky.dev@gmail.com>
2023-08-23 15:13:47 +0300

    TaskComponent unit test

--- kanban-fire/src/app/feature-swim-lanes/task/task.component.spec.ts
+++ kanban-fire/src/app/feature-swim-lanes/task/task.component.spec.ts
@@ -1,21 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatCardModule } from '@angular/material/card';
 import { TaskComponent } from './task.component';
+import { Task } from './task';
 
 describe('TaskComponent', () => {
   let component: TaskComponent;
   let fixture: ComponentFixture<TaskComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TaskComponent],
+      imports: [MatCardModule],
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [TaskComponent]
-    });
     fixture = TestBed.createComponent(TaskComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display task title and description', () => {
+    const task: Task = {
+      id: '1',
+      title: 'Sample Task',
+      description: 'This is a sample task description.',
+    };
+
+    component.task = task;
+    fixture.detectChanges();
+
+    const cardElement: HTMLElement = fixture.nativeElement.querySelector('.item');
+    expect(cardElement.textContent).toContain('Sample Task');
+    expect(cardElement.textContent).toContain('This is a sample task description.');
   });
 });
